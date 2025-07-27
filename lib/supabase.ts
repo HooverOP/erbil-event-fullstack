9b45da74ee6069476a13b9a3af1ccee1a212de2e
Hoover <vache.shahin@gmail.com>
2025-07-27 16:10:36 +0300

    Basa de

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://gbogewgbjzmednprtpun.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdib2dld2dianptZWRucHJ0cHVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1ODQ0ODgsImV4cCI6MjA2ODE2MDQ4OH0.DajDBQ6U_c--azNM6ovjhuI_QE8HxNbjY2VChXXQL3s";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
